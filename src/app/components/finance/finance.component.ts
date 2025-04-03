3e691fb3a2eda5d4517b761709052b7fe181c5b0
muhsalah777 <muhamedwaelsalah@gmail.com>
2025-04-03 20:05:41 +0200

    ui b update

--- src/app/components/finance/finance.component.ts
+++ src/app/components/finance/finance.component.ts
@@ -1,14 +1,31 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import { LinkService } from '../../link.service';
+import {
+  NgbAlertModule,
+  NgbDatepickerModule,
+} from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { Select } from 'primeng/select';
+import { MultiSelectModule } from 'primeng/multiselect';
 
+interface paymentmethod {
+    name: string,
+    code: string
+}
+
+
+interface City {
+  name: string;
+  code: string;
+}
 
 @Component({
   selector: 'app-finance',
-  imports: [],
+  imports: [NgbDatepickerModule, NgbAlertModule, FormsModule, Select,MultiSelectModule],
   templateUrl: './finance.component.html',
   styleUrl: './finance.component.css'
 })
-export class FinanceComponent {
+export class FinanceComponent implements OnInit{
   marginLeft = 200; // Default margin
   isSidebarOpen = false;
 
@@ -17,6 +34,7 @@ export class FinanceComponent {
     this.linkService.isSidebarOpen$.subscribe((value) => {
       this.isSidebarOpen = value;
     });
+    this.todayDate = this.getTodayDate();
   }
 
   // Function to update margin based on isSidebarOpen
@@ -24,4 +42,43 @@ export class FinanceComponent {
     this.marginLeft = this.isSidebarOpen ? 100 : 200;
     return this.marginLeft;
   }
+
+
+  ///////////////////////////////////////////////
+
+  todayDate: string;
+
+
+  // Function to get today's date
+  getTodayDate(): string {
+    const today = new Date();
+    const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: 'long', day: 'numeric' };
+    return today.toLocaleDateString('en-US', options);  // You can change 'en-US' to the desired locale
+  }
+
+  cities: City[] | undefined;
+
+    selectedCity: City | undefined;
+
+    ngOnInit() {
+        this.cities = [
+            { name: 'New York', code: 'NY' },
+            { name: 'Rome', code: 'RM' },
+            { name: 'London', code: 'LDN' },
+            { name: 'Istanbul', code: 'IST' },
+            { name: 'Paris', code: 'PRS' }
+        ];
+        this.PaymentMethod = [
+          {name: 'InstaPay', code: 'IP'},
+          {name: 'Cash', code: 'Ca'},
+          {name: 'Visa', code: 'Vi'},
+          {name: 'Other', code: 'O'}
+      ];
+    }
+
+
+    PaymentMethod!: paymentmethod[];
+
+    selectedPaymentMethod!: paymentmethod[];
+
 }
--- src/app/components/orders/orders.component.ts
+++ src/app/components/orders/orders.component.ts
@@ -1,18 +1,26 @@
 import { Component } from '@angular/core';
 import { LinkService } from '../../link.service';
+import { Customer } from '../../customer';
+import { CustomerService } from '../../customer.service';
+import { TableModule } from 'primeng/table';
+import { CommonModule } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @Component({
   selector: 'app-orders',
-  imports: [],
+  imports: [TableModule, CommonModule, ButtonModule, HttpClientModule],
   templateUrl: './orders.component.html',
-  styleUrl: './orders.component.css'
+  styleUrl: './orders.component.css',
+  providers: [CustomerService],
+  standalone: true,
 })
 export class OrdersComponent {
   marginLeft = 200; // Default margin
   isSidebarOpen = false;
 
-  constructor(private linkService: LinkService) {
+  constructor(private linkService: LinkService,private customerService: CustomerService) {
     // Subscribe to changes in isSidebarOpen
     this.linkService.isSidebarOpen$.subscribe((value) => {
       this.isSidebarOpen = value;
@@ -24,4 +32,46 @@ export class OrdersComponent {
     this.marginLeft = this.isSidebarOpen ? 100 : 200;
     return this.marginLeft;
   }
+
+
+
+  ///////////////////////////////////////////////////////////////////
+
+
+
+
+  customers!: Customer[];
+
+    first = 0;
+
+    rows = 10;
+
+    ngOnInit() {
+        this.customerService.getCustomersLarge().then((customers) => (this.customers = customers));
+    }
+
+    next() {
+        this.first = this.first + this.rows;
+    }
+
+    prev() {
+        this.first = this.first - this.rows;
+    }
+
+    reset() {
+        this.first = 0;
+    }
+
+    pageChange(event: { first: number; rows: number; }) {
+      this.first = event.first;
+      this.rows = event.rows;
+  }
+
+    isLastPage(): boolean {
+        return this.customers ? this.first + this.rows >= this.customers.length : true;
+    }
+
+    isFirstPage(): boolean {
+        return this.customers ? this.first === 0 : true;
+    }
 }
